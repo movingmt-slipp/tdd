fda2999727494cc6456b27afc8ff7cefe97df8c6
Administrator <Administrator@192.168.0.104>
2014-03-11 22:16:54 +0900

    10:16

--- TDD01/src/main/java/net/slipp/study0402/tdd01/Money.java
+++ TDD01/src/main/java/net/slipp/study0402/tdd01/Money.java
@@ -4,15 +4,15 @@ package net.slipp.study0402.tdd01;
 public class Money
 {
 
-	public int amount = 5 * 2;
+	public int amount;
 
 	public Money(int amount) {
 		this.amount = amount * 2;
 	}
 
 	public void times(int i) {
-		// TODO Auto-generated method stub
-		
+		amount = amount * 2;
+		return 
 	}
 
 }
