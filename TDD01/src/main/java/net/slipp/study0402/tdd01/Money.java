8b691c2d84a801c9570bb5bfe9aaf25026979873
Alex Yang <movingmt@gmail.com>
2014-03-11 22:10:50 +0900

    테스트케이스 곱하기에 3을 2로 수

--- TDD01/src/main/java/net/slipp/study0402/tdd01/Money.java
+++ TDD01/src/main/java/net/slipp/study0402/tdd01/Money.java
@@ -4,10 +4,10 @@ package net.slipp.study0402.tdd01;
 public class Money
 {
 
-	public int amount = 10;
+	public int amount = 5 * 2;
 
-	public Money(int i) {
-		// TODO Auto-generated constructor stub
+	public Money(int amount) {
+		this.amount = amount * 2;
 	}
 
 	public void times(int i) {
--- TDD01/src/test/java/net/slipp/study0402/tdd01/MoneyTest.java
+++ TDD01/src/test/java/net/slipp/study0402/tdd01/MoneyTest.java
@@ -8,7 +8,7 @@ public class MoneyTest extends TestCase
 {
 	public void testMultiplication() {
 		Money five = new Money(5);
-		five.times(3);
+		five.times(2);
 		assertEquals(10, five.amount);
 	}
 }
