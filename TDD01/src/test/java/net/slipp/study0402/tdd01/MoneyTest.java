9f5e9cf1fe53fe674c9948cd02ecd43c87182b14
Alex Yang <movingmt@gmail.com>
2014-03-11 18:38:01 +0900

    테스트 한줄 추가 - Money five 객체 생

--- TDD01/src/test/java/net/slipp/study0402/tdd01/MoneyTest.java
+++ TDD01/src/test/java/net/slipp/study0402/tdd01/MoneyTest.java
@@ -7,6 +7,7 @@ import junit.framework.TestSuite;
 public class MoneyTest extends TestCase
 {
 	public void testMultiplication() {
+		Money five = new Money();
 		five.times(3);
 		assertEquals(10, five.amount);
 	}
