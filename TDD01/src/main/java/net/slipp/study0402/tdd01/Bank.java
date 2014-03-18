4ad5aeca1a28fa90a510805a6d59a8e49b7c96e0
Alex Yang <movingmt@gmail.com>
2014-03-18 21:28:04 +0900

    12장 진짜 완료.. 클래스 누락건 수정함.

---
+++ TDD01/src/main/java/net/slipp/study0402/tdd01/Bank.java
@@ -0,0 +1,9 @@
+package net.slipp.study0402.tdd01;
+
+public class Bank {
+
+	public Money reduce(Expression source, String to) {
+		return Money.dollar(10);
+	}
+
+}
---
+++ TDD01/src/main/java/net/slipp/study0402/tdd01/Expression.java
@@ -0,0 +1,5 @@
+package net.slipp.study0402.tdd01;
+
+public interface Expression {
+
+}
